7e850866d9ae15905c7ba181c5daf1034432b1a2
Miguel Corona <miguelalex_v9@hotmail.com>
2020-10-17 01:03:21 -0500

    Recorte de audios a 3 min

--- Django/CALL/views.py
+++ Django/CALL/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, HttpResponse
 import boto3
+from pydub import AudioSegment
+
+
 
 # Cada funcion es una vista de la aplicacion web
 
 s3 = boto3.resource('s3')
-
+time = 180 * 1000  # segundos a milisegundos
 
 def home(request):
 
     if request.method == 'POST':
         files = request.FILES.getlist('files')
         for file in files:
-            file_name = 'calls/' + str(file)
-            s3.Bucket('django-call-storage').put_object(Key=file_name, Body=file)
+
+            sound = AudioSegment.from_file(file.file.name)
+            first_3_min = sound[:time]
+            # create a new file "first_half.mp3":
+            file_name = 'calls/' + str(file).replace(" ", "")
+            temp = first_3_min.export(format='wav')
+            temp.seek(0)
+
+            s3.Bucket('django-call-storage').put_object(Key=file_name, Body=temp)
 
     return render(request, 'CALL/home.html')
 
